9ebcb182ad6cd7984dcfd0ce14a3bda246e43453
sanketmodi <sanket.modi1@olacabs.com>
2022-10-05 01:18:14 +0530

    Fixes for submission

--- src/main/java/com/navi/assignment/Main.java
+++ src/main/java/com/navi/assignment/Main.java
@@ -1,20 +1,23 @@
 package com.navi.assignment;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.net.URL;
-import java.util.Objects;
+import java.io.StringReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        URL resource = Main.class.getClassLoader().getResource("test.txt");
-        assert resource!=null;
-        String filePath = resource.getFile();
-        if(args.length > 1 && Objects.equals(args[0], "-f")){
-            filePath = args[1];
-        }
+        String input ="ADD_BRANCH B1 CAR,BIKE,VAN\n" +
+                "ADD_VEHICLE B1 CAR V1 500\n" +
+                "ADD_VEHICLE B1 CAR V2 1000\n" +
+                "ADD_VEHICLE B1 BIKE V3 250\n" +
+                "ADD_VEHICLE B1 BIKE V4 300\n" +
+                "ADD_VEHICLE B1 BUS V5 2500\n" +
+                "BOOK B1 VAN 1 5\n" +
+                "BOOK B1 CAR 1 3\n" +
+                "BOOK B1 BIKE 2 3\n" +
+                "BOOK B1 BIKE 2 5\n" +
+                "DISPLAY_VEHICLES B1 1 5";
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+        try(BufferedReader reader = new BufferedReader(new StringReader(input))){
             InputProcessor inputProcessor = new InputProcessor();
             inputProcessor.process(reader);
         }
