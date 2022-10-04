e2694a15b69c1957892e1a92b7cc3e080ed91e70
sanketmodi <sanket.modi1@olacabs.com>
2022-10-05 01:10:43 +0530

    Fixes for submission

--- src/main/java/com/navi/assignment/Main.java
+++ src/main/java/com/navi/assignment/Main.java
@@ -2,11 +2,14 @@ package com.navi.assignment;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.net.URL;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String filePath = "src/main/resources/test.txt";
+        URL resource = Main.class.getClassLoader().getResource("test.txt");
+        assert resource!=null;
+        String filePath = resource.getFile();
         if(args.length > 1 && Objects.equals(args[0], "-f")){
             filePath = args[1];
         }
